7f44ed13b13f9757b3bf3c4192f75612c50cad84
gtheller <9164775+gtheller@users.noreply.github.com>
2018-04-01 19:58:32 -0400

    Combined estop and limiter code for uno

---
+++ estop_limiter.ino
@@ -0,0 +1,82 @@
+#define wiredPin 2
+#define wirelessPin 3
+#define limiterPin 4
+#define stopPin 5
+
+double pi = 3.1415926535;
+double radius = 6.5;
+double circ = 2.0*pi*radius;
+int curr = 0;
+int last = 0;
+long tic = 0;
+long toc = 0;
+long numEdges = 0;
+double numRot;
+double dist;
+long interval = 1000000;//micros
+long dt = 0;
+double maxSpeed = 5;
+double maxDist = maxSpeed*63360/3600*dt;
+double maxRot = maxDist/circ;
+
+int wiredSig = LOW;
+int wirelessSig = LOW;
+int limiterSig = LOW;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(wiredPin, INPUT);
+  pinMode(wirelessPin, INPUT);
+  pinMode(limiterPin, INPUT);
+  pinMode(stopPin, OUTPUT);
+}
+
+void loop()
+{
+  wiredSig = digitalRead(wiredPin);
+  wirelessSig = digitalRead(wirelessPin);
+  
+  curr = digitalRead(limiterPin);
+  if(last==LOW && curr==HIGH)
+  {
+    numEdges++;
+  }
+  last = curr;
+
+  toc = micros();
+  dt = toc - tic;
+  if(dt>=interval)
+  {
+    numRot = numEdges/360.0;
+    dist = circ*numRot;
+    maxDist = 5*63360/3600*dt/pow(10,6);
+    Serial.println("numEdges: " + String(numEdges));
+    Serial.println("numRot: " + String(numRot));
+    Serial.println("circ: " + String(circ));
+    Serial.println("dist: " + String(dist));
+    Serial.println("maxDist: " + String(maxDist));
+    Serial.println();
+    if(dist>maxDist)
+    {
+      limiterSig = HIGH;
+    }
+    else
+    {
+      limiterSig = LOW;      
+    }
+        
+    numEdges = 0;
+    tic = toc;
+  }
+
+  if(wiredSig || wirelessSig || limiterSig)
+  {
+    digitalWrite(stopPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(stopPin, LOW);
+  }
+
+}
